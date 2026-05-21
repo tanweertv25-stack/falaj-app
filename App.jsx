4bf5029dd16b0fb2963f164f88fb9c610c542853
tanweertv25-stack <tanweertv25@gmail.com>
2026-05-22 02:26:39 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,16 @@
-export { default } from "./src/App.jsx";
+export default function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#0f172a',
+      color: 'white',
+      fontSize: '30px',
+      fontWeight: 'bold'
+    }}>
+      نظام إدارة الفلج يعمل بنجاح ✅
+    </div>
+  )
+}

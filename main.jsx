266a073996cd29a1d7c7981fa6e2330afcbc0361
tanweertv25-stack <tanweertv25@gmail.com>
2026-05-22 02:36:18 +0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default function App() {
+  return (
+    <div>
+      <h1>الموقع يعمل الآن ✅</h1>
+    </div>
+  )
+}
